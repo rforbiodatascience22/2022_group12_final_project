252c524d3f105a8ce361ee14528efdbcce3d2106
jfunk-biodesign <funk.jonathan21@gmail.com>
2022-04-12 11:01:31 +0200

    loading data

--- R/01_load.R
+++ R/01_load.R
@@ -1,5 +1,11 @@
 # Load libraries ----------------------------------------------------------
-library("tidyverse")
+library(tidyverse)
+library(readr)
+library(dplyr)
+library(stringr)
+library(purrr)
+library(tidyr)
+library(ggplot2)
 
 
 # Define functions --------------------------------------------------------
@@ -7,8 +13,9 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_raw <- read_tsv(file = "data/_raw/my_raw_data.tsv")
-
+entries_raw <- read_tsv(file = "data/_raw/entries.idx")
+pdb_entry_type_raw <- read_tsv(file = "data/_raw/pdb_entry_type.txt")
+source_raw <- read_tsv(file = "data/_raw/source.idx")
 
 # Wrangle data ------------------------------------------------------------
 my_data <- my_data_raw # %>% ...
