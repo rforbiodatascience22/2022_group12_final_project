c52f6fdc8bb40f5815d13e0804d99689ffcad4df
jfunk-biodesign <funk.jonathan21@gmail.com>
2022-04-12 12:20:05 +0200

    removed unecessary space

--- R/01_load.R
+++ R/01_load.R
@@ -18,13 +18,10 @@ pdb_entry_type_raw <- read_tsv(file = "data/_raw/pdb_entry_type.txt")
 source_raw <- read_tsv(file = "data/_raw/source.idx", skip = 4, col_names = FALSE)
 
 
-
 # Wrangle data ------------------------------------------------------------
 
 # Loading column names into vector
 colnames(entries_raw) <- read.table(file = 'data/_raw/entries.idx',header = F,nrows = 1, sep = ',')[1,]
-
-# split source into columns
 
 source_raw <- wrangle_source(source_raw = source_raw)
 #my_data <- my_data_raw # %>% ...
