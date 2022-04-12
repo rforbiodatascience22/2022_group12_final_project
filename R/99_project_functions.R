ba1d39688888d88b49eef2f99c25ae2133701254
jfunk-biodesign <funk.jonathan21@gmail.com>
2022-04-12 20:56:33 +0200

    changed syntax to leons convention

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -5,9 +5,12 @@ wrangle_source <- function(source_raw){
   IDCODE <- c()
   SOURCE <- c()
   for (i in 1:length(rows)) {
-    IDCODE[i] <- substring(rows[i], 1, 4)
-    SOURCE[i] <- substring(rows[i], 5) %>%
-      str_replace_all("\t" ,"")
+    IDCODE[i] <- substring(rows[i], 
+                           first = 1, 
+                           last = 4)
+    SOURCE[i] <- substring(rows[i], 
+                           first = 5) %>%
+      str_replace_all("\t", "")
   }
   df <- data.frame(IDCODE  = IDCODE,
                    SOURCE = SOURCE)
