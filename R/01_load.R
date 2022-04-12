d52fd83e2cb1c2f3aa0651a211552c9d8baa36d0
jfunk-biodesign <funk.jonathan21@gmail.com>
2022-04-12 20:44:50 +0200

    removed unecessary comments

--- R/01_load.R
+++ R/01_load.R
@@ -19,14 +19,7 @@ source_raw <- read_tsv(file = "data/_raw/source.idx", skip = 4, col_names = FALS
 
 
 # Wrangle data ------------------------------------------------------------
-
-# Loading column names into vector
 colnames(entries_raw) <- read.table(file = 'data/_raw/entries.idx',header = F,nrows = 1, sep = ',')[1,]
 
 source_raw <- wrangle_source(source_raw = source_raw)
-#my_data <- my_data_raw # %>% ...
 
-
-# Write data --------------------------------------------------------------
-#write_tsv(x = my_data,
-#          file = "data/01_my_data.tsv")
