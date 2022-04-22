2d5627619e7ebd0f7e0bda14d76f0c93f0c8c24c
Ericbf-10 <ericbf10@gmail.com>
2022-04-22 13:36:15 +0000

    Remove source_raw wrangling

--- R/01_load.R
+++ R/01_load.R
@@ -15,13 +15,10 @@ source(file = "R/99_project_functions.R")
 # Load data ---------------------------------------------------------------
 entries_raw <- read_tsv(file = "data/_raw/entries.idx", skip = 2, col_names = FALSE)
 pdb_entry_type_raw <- read_tsv(file = "data/_raw/pdb_entry_type.txt")
-source_raw <- read_tsv(file = "data/_raw/source.idx", skip = 4, col_names = FALSE)
 
 
 # Wrangle data ------------------------------------------------------------
 colnames(entries_raw) <- read.table(file = 'data/_raw/entries.idx',header = F,nrows = 1, sep = ',')[1,]
-
-source_raw <- wrangle_source(source_raw = source_raw)
 
 
 
@@ -30,5 +27,3 @@ write_tsv(x = entries_raw,
           file = "data/01_entries.tsv")
 write_tsv(x = pdb_entry_type_raw,
           file = "data/01_pdb_entry_type.tsv")
-write_tsv(x = source_raw,
-          file = "data/01_source.tsv")
