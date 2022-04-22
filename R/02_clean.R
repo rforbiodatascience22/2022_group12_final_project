ddb61be3c3018f44ffb87036df3730eb43c92b1e
lauramago <laurasofiamago@gmail.com>
2022-04-22 17:06:26 +0000

    comment on next steps

--- R/02_clean.R
+++ R/02_clean.R
@@ -17,7 +17,10 @@ pdb_entries <- read_tsv(file = "data/01_dat_load.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-
+#1. merge experiment type columns in one that contain info of all entries (use mutate)
+#2. homogenize NAs (?)
+#3. subset data that contains source 
+#4. create a column year/decade we can use to create nice plot of "new entries per year per species (top 5)" <- line or density plot
 
 
 # Write data --------------------------------------------------------------
