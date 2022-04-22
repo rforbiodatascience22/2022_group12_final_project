2138af54b931dab1facdead4422729d608ad92ff
Ericbf-10 <ericbf10@gmail.com>
2022-04-22 14:43:34 +0000

    Add data wrangling and comments

--- R/01_load.R
+++ R/01_load.R
@@ -13,14 +13,28 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-entries_raw <- read_tsv(file = "data/_raw/entries.idx", skip = 2, col_names = FALSE)
-pdb_entry_type_raw <- read_tsv(file = "data/_raw/pdb_entry_type.txt")
+# Load entries.idx, skip first 2 lines
+entries_tsv <- read_tsv(file = "data/_raw/entries.idx", 
+                        skip = 2, 
+                        col_names = FALSE)
+
+# Load pdb_entry_type.txt and set col_names manually
+pdb_entry_type_tsv <- read_tsv(file = "data/_raw/pdb_entry_type.txt", 
+                               col_names = c("IDCODE", "MOLECULE TYPE", "EXPERIMENT TYPE"))
 
 
 # Wrangle data ------------------------------------------------------------
-colnames(entries_raw) <- read.table(file = 'data/_raw/entries.idx',header = F,nrows = 1, sep = ',')[1,]
+# Set col_names of entries_tsv
+colnames(entries_tsv) <- read.table(file = 'data/_raw/entries.idx',
+                                    header = FALSE,
+                                    nrows = 1, 
+                                    sep = ',')[1,]
 
+# Change letter of IDCODE to upper in pdb_entry_type_tsv
+pdb_entry_type_tsv <- pdb_entry_type_tsv %>% 
+  mutate(IDCODE = str_to_upper(IDCODE))
 
+# Join the 2 tables by IDCODE
 
 # Write data --------------------------------------------------------------
 write_tsv(x = entries_raw,
