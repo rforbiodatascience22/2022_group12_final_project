b0d43747c7a97818a4a27c130838804edc77c728
jfunk-biodesign <funk.jonathan21@gmail.com>
2022-04-25 09:15:33 +0200

    changed load

--- R/01_load.R
+++ R/01_load.R
@@ -13,24 +13,33 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-entries_raw <- read_tsv(file = "data/_raw/entries.idx", skip = 2, col_names = FALSE)
-pdb_entry_type_raw <- read_tsv(file = "data/_raw/pdb_entry_type.txt")
-source_raw <- read_tsv(file = "data/_raw/source.idx", skip = 4, col_names = FALSE)
+# Load entries.idx, skip first 2 lines
+entries_tsv <- read_tsv(file = "data/_raw/entries.idx", 
+                        skip = 2, 
+                        col_names = FALSE)
 
+# Load pdb_entry_type.txt and set col_names manually
+pdb_entry_type_tsv <- read_tsv(file = "data/_raw/pdb_entry_type.txt", 
+                               col_names = c("IDCODE", "MOLECULE TYPE", "EXPERIMENT TYPE"))
 
 
 # Wrangle data ------------------------------------------------------------
+# Set col_names of entries_tsv
+colnames(entries_tsv) <- read.table(file = 'data/_raw/entries.idx',
+                                    header = FALSE,
+                                    nrows = 1, 
+                                    sep = ',')[1,] %>% 
+  str_remove(pattern = " ")
 
-# Loading column names into vector
-entries_header <- read.table(file = 'data/_raw/entries.idx',header = F,nrows = 1, sep = ',')[1,]
-colnames(entries_raw) <- entries_header
+# Change letter of IDCODE to upper in pdb_entry_type_tsv
+pdb_entry_type_tsv <- pdb_entry_type_tsv %>% 
+  mutate(IDCODE = str_to_upper(IDCODE))
 
-# split source into columns
-
-source_raw <- wrangle_source(source_raw = source_raw)
-#my_data <- my_data_raw # %>% ...
-
+# Join the 2 tables by IDCODE
+pdb_entries <- entries_tsv %>% 
+  full_join(pdb_entry_type_tsv,
+            by = "IDCODE")
 
 # Write data --------------------------------------------------------------
-#write_tsv(x = my_data,
-#          file = "data/01_my_data.tsv")
+write_tsv(x = pdb_entries,
+          file = "data/01_dat_load.tsv")
