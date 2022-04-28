1fad9dd1df5168dfb8045c0ddb8769c87450e08b
Ericbf-10 <ericbaufa10@gmail.com>
2022-04-28 15:37:32 +0000

    Remove unnecessary file

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -7,7 +7,7 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
+pdb_entries_aug <- read_tsv(file = "data/03_dat_augment.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
