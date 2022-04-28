82ca9862a340b92e2d5b525ee84b6db6a5290c3a
Ericbf-10 <ericbaufa10@gmail.com>
2022-04-28 15:33:18 +0000

    Remove taxid and resolution columns

--- R/02_clean.R
+++ R/02_clean.R
@@ -11,15 +11,15 @@ pdb_entries <- read_tsv(file = "data/01_dat_load.tsv",
                         na = c("NOT", "NA"))
 
 
-# Wrangle data ------------------------------------------------------------
+# Wrangle data --------------------------------------------------------------
 #1. merge experiment type columns in one that contains info of all entries (use mutate)
-#2. homogenize NAs
 #3. subset data that contains source
 #4. create a column year/decade we can use to create "new entries per year per species" (line or density plot)
 
-# Clean data 
-#1. Remove the columns Author list and Experiment type.
-pdb_entries_clean <- select(pdb_entries, -c('AUTHOR LIST', 'EXPERIMENT TYPE'))
+# Remove unnecessary columns
+pdb_entries_clean <- pdb_entries %>% 
+  select(-c(`AUTHOR LIST`,`EXPERIMENT TYPE`,RESOLUTION,taxid))
+
 
 # Write data --------------------------------------------------------------
 write_tsv(x = pdb_entries_clean,
--- R/03_augment.R
+++ R/03_augment.R
@@ -7,14 +7,14 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean <- read_tsv(file = "data/02_my_data_clean.tsv")
+pdb_entries_clean <- read_tsv(file = "data/02_my_data_clean.tsv")
 
 #Change accession date to year
-date_to_year <- format(as.Date(my_data_clean$`ACCESSION DATE`, '%m/%d/%y'), "%Y")
-my_data_clean$`ACCESSION DATE` <- date_to_year
+date_to_year <- format(as.Date(pdb_entries_clean$`ACCESSION DATE`, '%m/%d/%y'), "%Y")
+pdb_entries_clean$`ACCESSION DATE` <- date_to_year
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug <- my_data_clean # %>% ...
+pdb_entries_aug <- pdb_entries_clean # %>% ...
 
 
 # Write data --------------------------------------------------------------
