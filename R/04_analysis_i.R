efd7d76e0cb1f0da83dca1466d4bcee73153bbdf
Ericbf-10 <ericbaufa10@gmail.com>
2022-04-28 16:05:00 +0000

    Implement basic taxonomy bar plot

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -11,15 +11,25 @@ pdb_entries_aug <- read_tsv(file = "data/03_dat_augment.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug %>% ...
+## TAXONOMY ANALYSIS ##
+pdb_taxonomy <- pdb_entries_aug %>% 
+  select(IDCODE, superkingdom, `MOLECULE TYPE`) %>% 
+  group_by(superkingdom) %>% 
+  count()
+pdb_taxonomy
 
 
 # Model data
-my_data_clean_aug %>% ...
+# my_data_clean_aug %>% ...
 
 
 # Visualise data ----------------------------------------------------------
-my_data_clean_aug %>% ...
+pdb_taxonomy %>% 
+  ggplot(mapping = aes(x = fct_reorder(superkingdom, 
+                                       desc(n)),
+                       y = n,
+                       fill = superkingdom)) +
+  geom_col()
 
 
 # Write data --------------------------------------------------------------
