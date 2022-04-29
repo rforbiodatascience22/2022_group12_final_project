4d8f5645924da7c63b07d2df387270cae4a626ee
Ericbf-10 <ericbaufa10@gmail.com>
2022-04-29 10:10:56 +0000

    Create second taxonomy plot

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -11,13 +11,30 @@ pdb_entries_aug <- read_tsv(file = "data/03_dat_augment.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
+#######################
 ## TAXONOMY ANALYSIS ##
-pdb_taxonomy <- pdb_entries_aug %>% 
+#######################
+
+# Select superkingdom and molecule type columns, then tidy
+taxonomy_df <- pdb_entries_aug %>% 
   select(IDCODE, superkingdom, `MOLECULE TYPE`) %>% 
-  group_by(superkingdom) %>% 
-  count(sort = TRUE) %>% 
   replace_na(list(superkingdom = "Others"))
+
+# Count number of entries per superkingdom
+pdb_taxonomy <- taxonomy_df %>% 
+  group_by(superkingdom) %>% 
+  add_tally(name = "n",
+            sort = TRUE) %>% 
+  distinct(superkingdom, n)
 pdb_taxonomy
+
+# Count number of entries per superkingdom stratified by molecule type
+pdb_taxa_mol <- taxonomy_df %>% 
+  group_by(superkingdom, `MOLECULE TYPE`) %>% 
+  add_tally(name = "n") %>% 
+  distinct(superkingdom, `MOLECULE TYPE`, n) %>% 
+  arrange(superkingdom)
+pdb_taxa_mol
 
 
 # Model data
@@ -25,6 +42,7 @@ pdb_taxonomy
 
 
 # Visualise data ----------------------------------------------------------
+# PDB Data Distribution By Superkingdom
 pdb_taxonomy %>% 
   ggplot(mapping = aes(x = factor(superkingdom, 
                                   level = c("Eukaryota", "Bacteria", "Viruses", "Archaea", "Others")),
@@ -35,10 +53,27 @@ pdb_taxonomy %>%
   scale_fill_discrete(breaks = c("Eukaryota", "Bacteria", "Viruses", "Archaea", "Others")) +
   theme_linedraw() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  labs(title = "PDB Data Distribution By Taxonomy",
+  labs(title = "PDB Data Distribution By Superkingdom",
        x = "Superkingdom",
        y = "Number of entries",
        fill = "Superkingdom")
+
+pdb_taxa_mol %>% 
+  ggplot(mapping = aes(x = factor(superkingdom,
+                                  level = c("Eukaryota", "Bacteria", "Viruses", "Archaea", "Others")),
+                       y = n,
+                       fill = superkingdom,
+                       alpha = `MOLECULE TYPE`)) +
+  geom_col() +
+  scale_y_continuous(breaks = seq(0,120000,10000)) +
+  scale_fill_discrete(breaks = c("Eukaryota", "Bacteria", "Viruses", "Archaea", "Others")) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "PDB Data Distribution By Superkingdom",
+       x = "Superkingdom",
+       y = "Number of entries",
+       fill = "Superkingdom",
+       alpha = "Superkingdom")
 
 
 # Write data --------------------------------------------------------------
