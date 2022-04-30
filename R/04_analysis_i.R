38d9072c47eb6338bf07dc876e1ad14d9d8a266a
Ericbf-10 <ericbaufa10@gmail.com>
2022-04-30 07:59:15 +0000

    Update second taxonomy plot

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -86,7 +86,8 @@ pdb_taxa_mol %>%
        y = "Number of entries",
        fill = "Molecule type")
 ggsave(filename = "results/pdb_taxa_mol.png",
-       height = 5)
+       height = 5,
+       width = 6.27)
 
 # Write data --------------------------------------------------------------
 write_tsv(...)
