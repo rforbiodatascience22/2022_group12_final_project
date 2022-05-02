0563c5211685dd83a0a07ad9ea46b5080f7d2d66
DeepthaSri <s210230@student.dtu.dk>
2022-05-02 08:36:37 +0000

    Changes to recreated plots

--- tmp/Bar_plots.R
+++ tmp/Bar_plots.R
@@ -1,14 +1,15 @@
 #Let's try this again
-protein <- str_replace(pdb_entries_clean$'MOLECULE TYPE', "prot", "PROTEIN")
-           pdb_entries_clean$'MOLECULE TYPE' <- protein
-nucleotide <- str_replace(pdb_entries_clean$'MOLECULE TYPE', "nuc", "NUCLEOTIDE")
-              pdb_entries_clean$'MOLECULE TYPE' <- nucleotide
+#protein <- str_replace(pdb_entries_clean$'MOLECULE TYPE', "prot", "PROTEIN")
+ #          pdb_entries_clean$'MOLECULE TYPE' <- protein
+#nucleotide <- str_replace(pdb_entries_clean$'MOLECULE TYPE', "nuc", "NUCLEOTIDE")
+ #             pdb_entries_clean$'MOLECULE TYPE' <- nucleotide
 
 #Plot 1 - Entity type bar plot
-molecule_bar <- pdb_entries_clean %>% 
-                drop_na('MOLECULE TYPE') %>% 
+molecule_bar <- pdb_entries_aug %>% 
+                drop_na(`MOLECULE TYPE`) %>% 
+  filter(`MOLECULE TYPE` != "other") %>% 
   ggplot(mapping = aes(x = `MOLECULE TYPE`)) +
-  geom_bar() +
+  geom_bar() + 
   theme_linedraw() +
   labs(title = "Distribution of Structures by Entity Type",
        x = "Entity Type",
@@ -17,12 +18,13 @@ molecule_bar <- pdb_entries_clean %>%
 molecule_bar  
 
 #Plot2 - Distribution of structures based on entry year
-accession_year_bar <- pdb_entries_clean %>% 
-                      drop_na('ACCESSION DATE') %>%  
-  ggplot(mapping = aes(y = `ACCESSION DATE`)) +
+accession_year_bar <- pdb_entries_aug %>% 
+                      drop_na(YEAR) %>%  
+  ggplot(mapping = aes(x = YEAR)) +
   geom_bar() +
-  theme_linedraw() +
-  labs(title = "Distribution of Structures by Date of Entry into PDB",
+  theme_linedraw() + 
+  theme(axis.text.x = element_text(angle = 0, hjust = 1)) +
+  labs(title = "Distribution of Structures by Year of Entry into PDB",
        x = "",
        y = "Accession year")
 
@@ -33,7 +35,7 @@ experiment_bar <- pdb_entries_aug %>%
   drop_na('EXPERIMENT TYPE (IF NOT X-RAY)') %>%  
   ggplot(mapping = aes(y = 'EXPERIMENT TYPE (IF NOT X-RAY)')) +
   geom_bar() +
-  theme_linedraw() +
+  theme_linedraw() + 
   labs(title = "Distribution of Structures based on Experiment type",
        x = "",
        y = "Experiment type")
@@ -43,10 +45,16 @@ experiment_bar
 
 #Plot for source needs more cleaning 
 
-source_bar <- pdb_entries_clean %>% str_replace('SOURCE', "; ", "") %>% 
-drop_na('SOURCE') %>% 
-ggplot(mapping = aes(x = 'SOURCE')) +
-geom_bar() +
-theme_minimal()
+source_bar <- pdb_entries_aug %>% #str_replace('SOURCE', "; ", "") %>% 
+              group_by(SOURCE) %>%
+              drop_na() %>% 
+              summarise(n = n()) %>% 
+              top_n(10) %>% 
+              mutate(REORDERED = reorder(SOURCE, desc(n))) %>%
+ggplot(mapping = aes(x = REORDERED, y = n)) +
+geom_bar(stat = "identity") +
+theme_minimal() +
+theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
 
 source_bar
