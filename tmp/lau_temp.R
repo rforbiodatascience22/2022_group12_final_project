3998f36ac3912b4256e3578dbcf3a83fc9c90836
lauramago <laurasofiamago@gmail.com>
2022-05-02 16:02:24 +0000

    create scop plot

--- tmp/lau_temp.R
+++ tmp/lau_temp.R
@@ -1,18 +1,29 @@
-#Clean scop column
-temp <- pdb_entries_aug %>% 
-  mutate(SCOP = str_match(SCOP,"CL=[\\d]+"),
-         SCOP = str_match(SCOP, "[\\d]+")) 
+# SCOP wrangling before plot
+scop_plot <- pdb_entries_aug %>% 
+  filter(`MOLECULE TYPE` != "nuc") %>% 
+  select(IDCODE, `MOLECULE TYPE`, SCOP_NAME) %>% 
+  drop_na(SCOP_NAME) %>% 
+  group_by(SCOP_NAME) %>% 
+  count() %>% 
+  ggplot(mapping = aes(x = fct_reorder(SCOP_NAME,
+                                       desc(n),
+                                       max),
+                       y = n,
+                       fill = SCOP_NAME)) +
+  geom_col() +
+  scale_x_discrete(labels = c("All alpha proteins" = "all-α",
+                              "All beta proteins" = "all-β",
+                              "Alpha and beta proteins (a/b)" = "α/β", 
+                              "Alpha and beta proteins (a+b)" = "α+β",
+                              "Small proteins" = "small")) +
+  geom_label(aes(label = n),
+             show.legend = FALSE) +
+  theme_linedraw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Distribution of SCOP classes in PDB entries",
+       x = "SCOP class",
+       y = "Number of entries",
+       fill = "SCOP class")
+#ggsave(filename = "results/pdb_scop.png")
 
-# Count number of entries per scop-reference
-pdb_taxonomy <- taxonomy_df %>% 
-  group_by(superkingdom) %>% 
-  add_tally(name = "n",
-            sort = TRUE) %>% 
-  distinct(superkingdom, n)
-pdb_taxonomy
-
-# Count number of entries per superkingdom
-temp <- pdb_entries_aug %>% 
-  select(`MOLECULE TYPE` = "prot") %>% 
-  group_by(SCOP_NAME)
-
+scop_plot
