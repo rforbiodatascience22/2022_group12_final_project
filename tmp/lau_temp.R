d8d90a90842c73ba24c85bc47ad1406102fc723c
lauramago <laurasofiamago@gmail.com>
2022-05-03 14:21:16 +0000

    finish plot

--- tmp/lau_temp.R
+++ tmp/lau_temp.R
@@ -11,13 +11,14 @@ scop_plot <- pdb_entries_aug %>%
                        y = n,
                        fill = SCOP_NAME)) +
   geom_col() +
+  geom_label(aes(label = n),
+             show.legend = FALSE) +
   scale_x_discrete(labels = c("All alpha proteins" = "all-α",
                               "All beta proteins" = "all-β",
                               "Alpha and beta proteins (a/b)" = "α/β", 
                               "Alpha and beta proteins (a+b)" = "α+β",
                               "Small proteins" = "small")) +
-  geom_label(aes(label = n),
-             show.legend = FALSE) +
+  scale_fill_brewer(palette = "Set1") +
   theme_linedraw() +
   theme(plot.title = element_text(hjust = 0.5)) +
   labs(title = "Distribution of SCOP classes in PDB entries",
