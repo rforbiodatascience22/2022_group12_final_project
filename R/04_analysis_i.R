7c2dc99539cbd4835a623c041cb487ae61fafcce
lauramago <laurasofiamago@gmail.com>
2022-05-05 17:23:17 +0000

    include scop plot in analysis

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -304,3 +304,43 @@ pdb_entries_aug %>%
        color = "Source Organism") 
 
 ggsave(filename = "results/source.png")
+
+
+# SCOP PLOTS ------------------
+
+# SCOP wrangling
+scop_df <- pdb_entries_aug %>% 
+  filter(`MOLECULE TYPE` != "nuc") %>% 
+  select(IDCODE, `MOLECULE TYPE`, SCOP_NAME) %>% 
+  drop_na(SCOP_NAME) %>% 
+  group_by(SCOP_NAME) %>% 
+  count()
+
+#SCOP plot
+scop_df %>% 
+  ggplot(mapping = aes(x = fct_reorder(SCOP_NAME,
+                                       desc(n),
+                                       max),
+                       y = n,
+                       fill = SCOP_NAME)) +
+  geom_col() +
+  #geom_label(aes(label = n),
+  #           show.legend = FALSE) +
+  scale_x_discrete(labels = c("Alpha and beta proteins (a+b)" = "α+β",
+                              "Alpha and beta proteins (a/b)" = "α/β",
+                              "All beta proteins" = "all-β", 
+                              "All alpha proteins" = "all-α",
+                              "Small proteins" = "small")) +
+  scale_fill_brewer(palette = "Set1",
+                    breaks = c("Alpha and beta proteins (a+b)",
+                               "Alpha and beta proteins (a/b)",
+                               "All beta proteins", 
+                               "All alpha proteins",
+                               "Small proteins")) +
+  theme_linedraw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Distribution of SCOP classes in PDB entries",
+       x = "SCOP class",
+       y = "Number of entries",
+       fill = "SCOP class")
+ggsave(filename = "results/pdb_scop.png")
